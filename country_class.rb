009ed1bb7cc224af39fbb620a4a7e9e0261a1ce6
Kristy Nelder <kristy.nelder@outlook.com>
2019-11-15 16:55:04 +1000

    added gems

---
+++ country_class.rb
@@ -0,0 +1,38 @@
+require "tty-table"
+require "colorize"
+
+class Country
+    attr_reader :phrases, :activities, :climate, :list, :language
+    def initialize(activities, phrases, climate, list, language)
+        @activities = activities
+        @phrases = phrases
+        @climate = climate
+        @list = list
+        @language = language
+    end
+
+    def to_do()
+        for activity in @activities do
+        puts activity.colorize(:red)
+        end
+    end
+
+    def translate()
+        heading = ["English", @language]
+        row = []
+        @phrases.each do |au, translation|
+            column = [au, translation]
+            row.push(column)   
+        end
+        table = TTY::Table.new(heading, row)
+        puts table.render(:unicode)
+    end
+
+    def climate()
+        puts @climate.colorize(:blue)
+    end
+
+    def packing()
+        puts @list
+    end
+end
